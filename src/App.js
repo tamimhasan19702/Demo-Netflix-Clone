3224dc762e61e4b32e1bc402815697155d8b1e74
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-03 01:46:48 +0600

    connected row component to appjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey there how are you!!!</h1>
+      <Row title="Netflix originals"/>
+      <Row title="Trending Now"/>
+
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Row({title}) {
+  return (
+    <div>
+        
+        {/* title */}
+        <h2>{title}</h2>
+
+        {/* container -> posters */}
+          
+    </div>
+  )
+}
+
+export default Row
