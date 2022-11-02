8b47284b58d11b378e4dca8f95abdd25651cb36b
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-03 01:59:52 +0600

    setting us app.js structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey there how are you!!!</h1>
-      <Row title="Netflix originals"/>
-      <Row title="Trending Now"/>
+      <Row title="Netflix originals" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
 
     </div>
   );
--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 
 function Row({title}) {
+
+    const [movies, setMovies] = useState([])
+
+
   return (
     <div>
         
