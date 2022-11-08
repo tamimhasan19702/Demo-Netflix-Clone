e36281fed74c329c3fce389466b55f1e71e57df4
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-08 16:00:50 +0600

    added multiple rows according to the movie genre

--- src/App.js
+++ src/App.js
@@ -8,9 +8,38 @@ function App() {
     <div className="App">
       <h1>Hey there how are you!!!</h1>
      
-      <Row title="Netflix originals" fetchUrl=  {requests.fetchNetflixOriginals}/>
+      <Row 
+      title="Netflix originals" 
+      fetchUrl=  {requests.fetchNetflixOriginals}/>
 
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row 
+      title="Trending Now" 
+      fetchUrl={requests.fetchTrending}/>
+
+      <Row
+      title="Top Rated" 
+      fetchUrl={requests.fetchTopRated}/>
+
+      <Row 
+      title="Action Movies" 
+      fetchUrl={requests.fetchActionMovies}/>
+
+      <Row 
+      title="Comedy Movies" 
+      fetchUrl={requests.fetchComedyMovies}/>
+
+      <Row 
+      title="Horror Movies" 
+      fetchUrl={requests.fetchHorrorMovies}/>
+
+      <Row 
+      title="Romance Movies" 
+      fetchUrl={requests.fetchRomanceMovies}/>
+
+      <Row 
+      title="Documentaries" 
+      fetchUrl={requests.fetchDocumentaries}/>
+
 
     </div>
   );
