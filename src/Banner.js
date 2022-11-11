267c2b3e01145dfdee8d287683908241a5d6f9c4
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-11 22:25:37 +0600

    useState add to bannerjs

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 function Banner() {
+  const [movie,setMovie] = useState([]);
+
   return (
-    <div>
-      
-    </div>
+    <header>
+        {/* background image */}
+        {/* title */}
+        {/* div > 2 buttons */}
+        {/* description */}
+    </header>
   )
 }
 
