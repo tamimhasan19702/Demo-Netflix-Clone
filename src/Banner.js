579eeac83d88e5eb7e8b2d0598c7e007225d12b3
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-11 22:28:56 +0600

    useEffect add

--- src/Banner.js
+++ src/Banner.js
@@ -3,6 +3,17 @@ import React, {useState} from 'react';
 function Banner() {
   const [movie,setMovie] = useState([]);
 
+
+//   useEffect is a special method which runs on a special condition
+
+  useEffect(() => {
+   async function fetchData(){
+
+   }
+   fetchData();
+  }, []);
+
+
   return (
     <header>
         {/* background image */}
