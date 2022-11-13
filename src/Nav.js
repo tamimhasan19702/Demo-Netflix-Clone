dd3cf98c53856343ed9de899564766aacceac5ab
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-13 23:14:16 +0600

    navbar css updates

--- src/Nav.js
+++ src/Nav.js
@@ -1,19 +1,20 @@
-import React ,{useEffect, useState} from 'react';
+import React ,{ useState, useEffect} from 'react';
 import './Nav.css';
 
 function Nav() {
-  const [show, handleShow] = useState(false)
+  const [show, handleShow] = useState(false);
 
+ 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if(window.scrollY > 100){
+    window.addEventListener("scroll" , () => {
+      if(window.scrollY > 110){
       handleShow(true);
       }else handleShow(false);
     });
     return () => {
-    window.removeEventListener('scroll');
+      
     }
-  }, []);
+  }) 
 
   return (
     <div className={`nav ${show && 'nav-black'}`}>
