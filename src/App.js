c04d95ca91f06ab6d99f92adcf483535ad675bb7
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-13 22:03:18 +0600

    header add

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Row from './Row';
 import requests from './requests';
 import Banner from './Banner';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
       
      {/* Nav */}
-
+     <Nav/>
 
      {/* banner */}
      <Banner/>
