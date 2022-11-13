c0350884760333dbdc7407b6bf9e4f353d1548fb
tamimhasan19702 <tamimhasancu@gmail.com>
2022-11-13 22:33:17 +0600

    navbar scroll add

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React ,{useEffect} from 'react';
 import './Nav.css';
 
 function Nav() {
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if(window.scrollY > 100){
+      handleShow(true);
+      }else handleShow(false);
+    });
+    return () => {
+    window.removeEventListener('scroll');
+    }
+  }, []);
+
   return (
     <div className='nav'>
 
@@ -14,7 +26,7 @@ function Nav() {
     src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745" 
     alt="nav-avatar" 
     className="nav-avatar" />
-    
+
     </div>
   )
 }
